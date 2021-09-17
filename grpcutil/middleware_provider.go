d344a426e9e3004398f9d39c3191ed4ec2f4dc78
viovanov <vlad@aserto.com>
2021-09-18 02:23:32 +0300

    Add middleware provider

---
+++ grpcutil/middleware_provider.go
@@ -0,0 +1,40 @@
+package grpcutil
+
+import (
+	grpc_middleware "github.com/grpc-ecosystem/go-grpc-middleware"
+	"google.golang.org/grpc"
+)
+
+type Middleware interface {
+	Unary() grpc.UnaryServerInterceptor
+	Stream() grpc.StreamServerInterceptor
+}
+
+type Middlewares []Middleware
+
+func (m Middlewares) AsGRPCOptions() []grpc.ServerOption {
+	unaryInterceptors := []grpc.UnaryServerInterceptor{}
+	streamInterceptors := []grpc.StreamServerInterceptor{}
+
+	for _, middleware := range m {
+		if m == nil {
+			continue
+		}
+
+		unaryInterceptor := middleware.Unary()
+		streamInterceptor := middleware.Stream()
+
+		if unaryInterceptor != nil {
+			unaryInterceptors = append(unaryInterceptors, unaryInterceptor)
+		}
+
+		if streamInterceptor != nil {
+			streamInterceptors = append(streamInterceptors, streamInterceptor)
+		}
+	}
+
+	return []grpc.ServerOption{
+		grpc.UnaryInterceptor(grpc_middleware.ChainUnaryServer(unaryInterceptors...)),
+		grpc.StreamInterceptor(grpc_middleware.ChainStreamServer(streamInterceptors...)),
+	}
+}
