9a81a03b600e71ea332d3fe53ba8428bbb098a2f
carabasdaniel <dani@aserto.com>
2022-08-25 14:43:36 +0300

    Extract authn and grpc into aserto-grpc

--- authn/auth0/config.go
+++
@@ -1,9 +0,0 @@
-package auth0
-
-type Config struct {
-	AcceptableTimeSkewSeconds int    `json:"acceptable_time_skew"`
-	Audience                  string `json:"audience"`
-	Domain                    string `json:"domain"`
-	ClientID                  string `json:"client_id"`
-	ClientSecret              string `json:"client_secret"`
-}
