84c16369d7707d2f061095b928e3c9245be3df3e
mihaibuzgau <mihaibuzgau@gmail.com>
2022-10-20 12:17:35 +0000

    remove errors tests

--- cerr/errors_test.go
+++ cerr/errors_test.go
@@ -7,9 +7,7 @@ import (
 
 	"github.com/aserto-dev/go-utils/cerr"
 	"github.com/stretchr/testify/require"
-	"google.golang.org/genproto/googleapis/rpc/errdetails"
 	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/status"
 )
 
 func TestDoubleCerr(t *testing.T) {
@@ -73,101 +71,5 @@ func TestWithGrpcStatusCode(t *testing.T) {
 func TestWithHttpStatusCode(t *testing.T) {
 	assert := require.New(t)
 	err := cerr.ErrAccountNotFound.WithHTTPStatus(http.StatusAccepted)
-	assert.Equal(err.HttpCode, http.StatusAccepted)
-}
-
-func TestFromGRPCStatus(t *testing.T) {
-	assert := require.New(t)
-
-	initialErr := cerr.ErrUserNotFound
-	initialErr = initialErr.Str("email", "testuser@aserto.com").Msg("foo")
-
-	grpcStatus := status.New(initialErr.StatusCode, initialErr.Error())
-	grpcStatus, err := grpcStatus.WithDetails(&errdetails.ErrorInfo{
-		Reason:   "1234",
-		Metadata: initialErr.Data(),
-		Domain:   initialErr.Code,
-	})
-
-	if err != nil {
-		assert.Fail(err.Error())
-	}
-
-	transformedErr := cerr.FromGRPCStatus(*grpcStatus)
-
-	assert.True(initialErr.SameAs(transformedErr))
-
-	assert.Equal(initialErr.Error(), transformedErr.Error())
-	assert.Equal(initialErr.Message, transformedErr.Message)
-}
-
-func TestUnwrapNilErr(t *testing.T) {
-	assert := require.New(t)
-
-	err := cerr.UnwrapAsertoError(nil)
-
-	assert.Nil(err)
-}
-
-func TestEquals(t *testing.T) {
-	assert := require.New(t)
-
-	err1 := cerr.ErrConnection.Msgf("error 1").Str("key1", "val1").Err(errors.New("boom"))
-	err2 := cerr.ErrConnection.Msgf("error 2").Str("key2", "val2").Err(errors.New("zoom"))
-
-	assert.True(cerr.Equals(err1, err2))
-
-}
-
-func TestEqualsNil(t *testing.T) {
-	assert := require.New(t)
-
-	assert.True(cerr.Equals(nil, nil))
-}
-
-func TestEqualsOneNil(t *testing.T) {
-	assert := require.New(t)
-
-	assert.False(cerr.Equals(cerr.ErrAccountNotFound, nil))
-}
-
-func TestEqualsNormalErrorOneNil(t *testing.T) {
-	assert := require.New(t)
-
-	assert.False(cerr.Equals(errors.New("boom"), nil))
-}
-
-func TestEqualsErrCerr(t *testing.T) {
-	assert := require.New(t)
-
-	assert.False(cerr.Equals(errors.New("boom"), cerr.ErrAccountNotFound))
-}
-
-func TestEqualsFalse(t *testing.T) {
-	assert := require.New(t)
-
-	assert.False(cerr.Equals(cerr.ErrAlreadyMember, cerr.ErrAccountNotFound))
-}
-
-func TestEqualsNormalErrors(t *testing.T) {
-	assert := require.New(t)
-
-	assert.False(cerr.Equals(errors.New("boom1"), errors.New("boom2")))
-}
-
-func TestCodeToAsertoError(t *testing.T) {
-	assert := require.New(t)
-
-	asertoErr := cerr.CodeToAsertoError("E10009")
-
-	assert.NotNil(asertoErr)
-	assert.True(cerr.Equals(asertoErr, cerr.ErrGithubAccessToken))
-}
-
-func TestCodeToAsertoErrorInvalidCode(t *testing.T) {
-	assert := require.New(t)
-
-	asertoErr := cerr.CodeToAsertoError("E20009")
-
-	assert.Nil(asertoErr)
+	assert.Equal(err.HTTPCode, http.StatusAccepted)
 }
